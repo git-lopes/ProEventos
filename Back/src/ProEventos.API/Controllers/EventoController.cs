f6f17cca669b6f62e01de1d7f2070cfeea881954
git-lopes <106499863+git-lopes@users.noreply.github.com>
2023-01-30 15:05:23 -0300

    Seção 2 .NET 6

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventoController : ControllerBase
+{
+ 
+     public EventoController( )
+    {
+     
+    }
+
+    [HttpGet]
+    public Evento Get()
+    {
+        return new Evento() {
+            EventoId = 1,
+            Tema = "Angular 11 e NET .6",
+            Local ="Fortaleza",
+            Lote = "1a. Lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString()
+        };
+        
+    }
+    
+}
